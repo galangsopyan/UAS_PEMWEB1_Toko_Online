1c80c74d878863ff71bbc40543606cfd1ad018ad
galang-eng <galangsofyan54@gmail.com>
2025-12-31 15:59:41 +0700

    Fix : Sinkronisasi akhir update

--- data/update.php
+++ data/update.php
@@ -1,17 +1,26 @@
 <?php
 include '../config/koneksi.php';
 
-$id     = $_POST['id'];
-$nama   = $_POST['nama_produk'];
-$harga  = $_POST['harga'];
-$stok   = $_POST['stok'];
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $nama = $_POST['nama_produk'];
+    $harga = $_POST['harga'];
+    $stok = $_POST['stok'];
+    
+    // Cek apakah ada file gambar baru yang diunggah
+    if ($_FILES['gambar']['name'] != "") {
+        $ekstensi = pathinfo($_FILES['gambar']['name'], PATHINFO_EXTENSION);
+        $nama_baru = time() . "." . $ekstensi;
+        move_uploaded_file($_FILES['gambar']['tmp_name'], "../assets/img/" . $nama_baru);
+        
+        $query = "UPDATE produk SET nama_produk='$nama', harga='$harga', stok='$stok', gambar='$nama_baru' WHERE id='$id'";
+    } else {
+        // Jika tidak ada gambar baru, jangan ubah kolom gambar
+        $query = "UPDATE produk SET nama_produk='$nama', harga='$harga', stok='$stok' WHERE id='$id'";
+    }
 
-$stmt = $koneksi->prepare("UPDATE produk SET nama_produk=?, harga=?, stok=? WHERE id=?");
-$stmt->bind_param("siii", $nama, $harga, $stok, $id);
-
-if ($stmt->execute()) {
-    header("Location: index.php?pesan=update_berhasil");
-} else {
-    echo "Gagal mengupdate data.";
+    if (mysqli_query($koneksi, $query)) {
+        header("Location: index.php?pesan=update_berhasil");
+    }
 }
 ?>
